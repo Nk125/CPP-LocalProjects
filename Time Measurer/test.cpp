4a89dafaeb5bd345ca2abbc5830450b735277214
nk125 <nekes125@outlook.com>
2021-11-05 00:44:42 -0600

    Update test.cpp

--- Time Measurer/test.cpp
+++ Time Measurer/test.cpp
@@ -1,6 +1,7 @@
 #include "multipurpose_time_measurer.hpp"
 #include <chrono>
 #include <iostream>
+#include <future>
 using namespace std;
 
 void await() {
@@ -9,14 +10,29 @@ void await() {
 }
 
 int main() {
-  nk125::measurer msr(10);
+  // This need to be too verbose, sorry if there's too much cout's
+  nk125::measurer ms;
+  cout << "Test 1 (get_ms_taken, no initialization before): " << ms.get_ms_taken() << endl;
+  cout << "Test 2 (end_timestamp, no initialization before)\n";
+  ms.end_timestamp();
+  cout << "Test 2 Finished\n";
   while (true) {
-    msr.start_timestamp();
+    cout << "Test 3 (Async start)\n";
+    async(launch::async, [&ms]{ms.start_timestamp();});
+    cout << "Test 3 Finished\n";
+    cout << "Awaiting...\n";
     await();
-    msr.end_timestamp();
-    cout << "Awaited " << msr.get_ms_taken() << " milliseconds\n";
-    cout << "Operations per microsecond: " << msr.op_per_micro() << endl;
-    cout << "Operations per millisecond: " << msr.op_per_ms() << endl;
-    cout << "Operations per second: " << msr.op_per_sec() << endl;
+    cout << "Awaited.\n";
+    cout << "Test 4 (Async ending)\n";
+    async(launch::async, [&ms]{ms.end_timestamp();});
+    cout << "Test 4 Finished\n";
+    cout << "Test 5 (Double ending timestamps)\n";
+    ms.end_timestamp();
+    cout << "Test 5 Finished\n";
+    cout << "Awaited " << ms.get_ms_taken() << " milliseconds\n";
+    cout << "Awaited " << ms.get_sec_taken() << " milliseconds\n";
+    cout << "Operations per microsecond: " << ms.op_per_micro() << endl;
+    cout << "Operations per millisecond: " << ms.op_per_ms() << endl;
+    cout << "Operations per second: " << ms.op_per_sec() << "\n\n";
   }
 }
