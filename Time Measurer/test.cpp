223310a4f931a57b07276c8c417052a46d9f4248
nk125 <nekes125@outlook.com>
2021-11-05 01:53:07 -0600

    Update test.cpp

--- Time Measurer/test.cpp
+++ Time Measurer/test.cpp
@@ -6,29 +6,30 @@ using namespace std;
 
 void await() {
   using namespace chrono_literals;
-  this_thread::sleep_for(2s);
+  this_thread::sleep_for(20ms);
 }
 
 int main() {
   // This need to be too verbose, sorry if there are too much cout's
   nk125::measurer ms;
   cout << "Test 1 (get_ms_taken, no initialization before): " << ms.get_ms_taken() << endl;
-  cout << "Test 2 (end_timestamp, no initialization before)\n";
+  cout << "Test 2 (op_per_ms, no initialization before): " << ms.op_per_ms() << endl;
+  cout << "Test 3 (end_timestamp, no initialization before)\n";
   ms.end_timestamp();
-  cout << "Test 2 Finished\n";
+  cout << "Test 3 Finished\n";
   while (true) {
-    cout << "Test 3 (Async start)\n";
+    cout << "Test 4 (Async start)\n";
     async(launch::async, [&ms]{ms.start_timestamp();});
-    cout << "Test 3 Finished\n";
+    cout << "Test 4 Finished\n";
     cout << "Awaiting...\n";
     await();
     cout << "Awaited.\n";
-    cout << "Test 4 (Async ending)\n";
+    cout << "Test 5 (Async ending)\n";
     async(launch::async, [&ms]{ms.end_timestamp();});
-    cout << "Test 4 Finished\n";
-    cout << "Test 5 (Double ending timestamps)\n";
-    ms.end_timestamp();
     cout << "Test 5 Finished\n";
+    cout << "Test 6 (Double ending timestamps)\n";
+    ms.end_timestamp();
+    cout << "Test 6 Finished\n";
     cout << "Awaited " << ms.get_ms_taken() << " milliseconds\n";
     cout << "Awaited " << ms.get_sec_taken() << " seconds\n";
     cout << "Operations per microsecond: " << ms.op_per_micro() << endl;
